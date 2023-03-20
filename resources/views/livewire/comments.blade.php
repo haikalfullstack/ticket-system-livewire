3740e6cd16d16c2815ab5ca5b5f3efe3da369eff
ekalrozhan <ekalrozhan@gmail.com>
2023-03-21 00:40:28 +0800

    Submit event

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,13 +1,15 @@
 <div>
     <h1>Comments</h1>
     <div class="">
-        <input type="text" wire:model="newComment">
-        <button wire:click="addComment">Add</button>
+        <form wire:submit.prevent="addComment()">
+            <input type="text" wire:model.lazy="newComment">
+            <button type="submit">Add</button>
+        </form>
     </div>
     {{ $newComment }}
     @foreach ($comments as $comment)
         <div style=" border: 1px solid grey; padding: 5px; max-width:500px;">
-            <div style="display:flex; ">
+            <div style="display:flex; flex-wrap:wrap;">
                 <p style="font-weight:bold; margin-right: 10px;">{{ $comment['creator'] }}</p>
                 <p>{{ $comment['created_at'] }}</p>
             </div>
